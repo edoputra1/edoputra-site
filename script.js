e212fc802f8dd4d1add9236f62ac7f0400b8ef7a
Edo Putra <hi.edoputra@gmail.com>
2026-01-24 20:34:14 +0700

    fix rem sizing

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ).join('');
 
     const chevronSVG = `
-      <svg width="40" height="40" viewBox="0 0 24 24" fill="none"
+      <svg viewBox="0 0 24 24" fill="none"
         xmlns="http://www.w3.org/2000/svg">
         <path
           d="M9 18L15 12L9 6"
